6f35eecd3d73a5fec3523f0c8d8d0b426a11bea2
Hari <thisismekrishnan@gmail.com>
2023-06-07 14:32:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import ProfileCard from "./ProfileCard";
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <div className="head">Personal Digital Assistant</div>
+      <div className="mainCard">
+        <ProfileCard title="Alexa" handle="@alexa123" />
+        <ProfileCard title="Cortana" handle="@cortana123" />
+        <ProfileCard title="Siri" handle="@siri123" />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/ProfileCard.js
@@ -0,0 +1,23 @@
+import './ProfileCard.css'
+
+const ProfileCard = (props) => {
+
+    const cardStyle = {
+        backgroundColor: '#f2f2f2',
+        padding: '10px',
+        borderRadius: '4px',
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+        margin :"10px"
+      };
+      const {title,handle} = props
+  return (
+    <>
+      <div className="card" style={cardStyle}>
+        <h4>title is {title} </h4>
+        <h4> handle is {handle}</h4>
+      </div>
+    </>
+  );
+};
+
+export default ProfileCard;
